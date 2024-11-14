66cc0558a67b13dbed1e0ba004156c4d27c718e2
eae22 <eun0110@dgu.ac.kr>
2024-11-15 07:12:52 +0900

    올라프 아이콘 추가

--- back/server.js
+++ back/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const path = require("path");
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-const db_config = require("./config/db_config.json");
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const db_config = require('./config/db_config.json');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 const sessionStoreOptions = {
   host: db_config.host,
@@ -12,7 +12,7 @@ const sessionStoreOptions = {
   user: db_config.user,
   password: db_config.password,
   database: db_config.database,
-  charset: "utf8mb4", // 여기서 utf8mb4 설정
+  charset: 'utf8mb4', // 여기서 utf8mb4 설정
 };
 
 const sessionStore = new MySQLStore(sessionStoreOptions);
@@ -22,16 +22,16 @@ app.use(express.json());
 app.use(cors());
 
 const sessionMiddleware = session({
-  key: "session_cookie_name",
-  secret: "sakdlakdjaslkdjsaldk",
+  key: 'session_cookie_name',
+  secret: 'sakdlakdjaslkdjsaldk',
   store: sessionStore,
   resave: false,
   saveUninitialized: false,
   rolling: true,
-  unset: "destroy",
+  unset: 'destroy',
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // 배포 환경에서는 true
+    secure: process.env.NODE_ENV === 'production', // 배포 환경에서는 true
     maxAge: 1000 * 60 * 60 * 24, // 1일
   },
 });
@@ -39,34 +39,42 @@ const sessionMiddleware = session({
 app.use(sessionMiddleware);
 
 // 정적 파일 제공
-app.use(express.static(path.join(__dirname, "../front/build")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '../front/build')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// React 앱의 빌드된 정적 파일 제공
+app.use(express.static(path.join(__dirname, '../front/build')));
+
+// favicon.ico 직접 제공
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/public', 'favicon.ico'));
+});
 
 //라우팅
-const yoloRoutes = require("./cctv/yolo"); // YOLO 라우터 추가
+const yoloRoutes = require('./cctv/yolo'); // YOLO 라우터 추가
 
 // 기존 라우터들
-const mypageRoutes = require("./user/mypage");
-const loginRoutes = require("./user/login");
-const logoutRoutes = require("./user/logout");
-const logincheckRoutes = require("./user/check-login");
-const lostlistRoutes = require("./lostpost/lostlist");
-const lostdetailRoutes = require("./lostpost/lostdetail");
-const requestRoutes = require("./lostpost/request");
+const mypageRoutes = require('./user/mypage');
+const loginRoutes = require('./user/login');
+const logoutRoutes = require('./user/logout');
+const logincheckRoutes = require('./user/check-login');
+const lostlistRoutes = require('./lostpost/lostlist');
+const lostdetailRoutes = require('./lostpost/lostdetail');
+const requestRoutes = require('./lostpost/request');
 
 // YOLO 라우터 추가
-app.use("/", mypageRoutes);
-app.use("/", loginRoutes);
-app.use("/", logincheckRoutes);
-app.use("/", logoutRoutes);
-app.use("/", yoloRoutes);
-app.use("/", lostlistRoutes);
-app.use("/", lostdetailRoutes);
-app.use("/", requestRoutes);
+app.use('/', mypageRoutes);
+app.use('/', loginRoutes);
+app.use('/', logincheckRoutes);
+app.use('/', logoutRoutes);
+app.use('/', yoloRoutes);
+app.use('/', lostlistRoutes);
+app.use('/', lostdetailRoutes);
+app.use('/', requestRoutes);
 
 //프론트 연결
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
